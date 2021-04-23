a4280a8b1bcc8b74a091cac360e9ef501a9128ce
Mirumo <mysteryperson520@gmail.com>
2021-04-23 20:07:26 +0800

    [feat] who is banned cant not display msg

--- r0-page/src/store/actions/message.js
+++ r0-page/src/store/actions/message.js
@@ -2,7 +2,7 @@ export const newMessage = (e) => (dispatch, getStore) => {
 	const { config } = getStore();
 	let { message } = getStore();
 	if (config.admin.indexOf(e.user_name) !== -1) e.auth = 'admin';
-
+	if (config.ban.indexOf(e.user_name) !== -1) return;
 	if (message.length >= config.messageBufferLength) message.shift();
 	message.push(e);
 	dispatch({
